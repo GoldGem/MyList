00e63c1d77b2f437af74a9311b8a5ee48835af0b
GoldGem <mishachemo@gmail.com>
2020-10-16 15:13:51 +0300

    one big commit
    add MyList
    add MyListT

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        MyList list = new MyList();
+        list.add(25);
+        list.add(5);
+        list.add(20);
+        list.add(21);
+        list.add(34);
+        list.add(55);
+        System.out.println(list.toString());
+//        list.swap(2, 4);
+//        System.out.println(list.toString());
+        list.sort();
+        System.out.println(list.toString());
+    }
+}
